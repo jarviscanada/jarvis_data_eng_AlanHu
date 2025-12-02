1c2eeb1bc3579724b5c4ab6ff012e32719774949
Alan Hu <alan.notiverson@gmail.com>
2025-12-02 16:01:32 +0000

    Add finished scripts host_info.sh and host_usage.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# assign command line positional arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+
+# check number of args
+if [ "$#" -ne 5  ]; then
+	echo "Illegal number of parameters"
+	exit 1
+fi
+
+# save machine statistics
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+# system hardware info
+lscpu_out=$(lscpu)
+cpu_number=$(echo "$lscpu_out" | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out" | egrep "Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out" | awk -F: '/L2/ {print $2}' | xargs)
+cpu_mhz=$(echo "$lscpu_out" | awk -F: '/Model name/ {
+    match($2, /([0-9.]+)GHz/, m);
+    if (m[1] != "") print m[1] * 1000;
+}')
+l2_cache=$(echo "$lscpu_out" | awk -F: '/L2/ {print $2}' | xargs | cut -d" " -f1)
+total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}')
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+insert_stmt="INSERT INTO host_info (
+  hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, \"timestamp\", total_mem
+) VALUES (
+  '$hostname', $cpu_number, '$cpu_architecture', '$cpu_model',
+  $cpu_mhz, $l2_cache, '$timestamp', '$total_mem'
+);"
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
---
+++ linux_sql/scripts/host_usage.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# assign positional arguments
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+# check number of parameters
+if [ "$#" -ne 5 ]; then
+	echo "Illegal number of parameters"
+	exit 1	
+fi
+
+# save machine statistics to variables
+vmstat_mb=$(vmstat --unit M)
+hostname=$(hostname -f)
+
+# fetch system usage data
+
+memory_free=$(echo "$vmstat_mb" | awk '{print $4}' | tail -n1 | xargs)
+cpu_idle=$(echo "$vmstat_mb" | tail -n1 | awk -v col="15" '{print $col}')
+cpu_kernel=$(echo "$vmstat_mb" | tail -n1 | awk -v col="14" '{print $col}')
+disk_io=$(echo "$vmstat_mb" | tail -n1 | awk -v col="10" '{print $col}')
+disk_available=$(df -m / | awk 'NR==2 {print $4}')
+
+# find timestamp
+timestamp=$(vmstat -t | tail -n1 | awk '{print $18, $19}')
+
+# subquery to find matching id in host_info table
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')"
+
+# insert statement 
+insert_stmt="INSERT INTO host_usage (\"timestamp\", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available) \
+VALUES ('$timestamp', $host_id, '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available');"
+
+#set up env var for pql cmd
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
